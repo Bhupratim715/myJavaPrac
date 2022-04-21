d546de7a097ca2e28e8f6bd32cb49382201272b1
Bhupratim715 <1802048@che.buet.ac.bd>
2022-04-22 03:00:08 +0600

    tring new

--- Person.java
+++ Person.java
@@ -1,12 +1,13 @@
 import java.util.Scanner;
-public class Person {
-  static Scanner sc = new Scanner(System.in);
-  public static void main(String[] args) {
-    System.out.println("please enter your name\n");
-    String name = sc.nextLine();
-    System.out.println("hello "+name);
-    
-    
-  }
-  
+
+public class Hello {
+	static Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("please enter your name \n");
+		String name = sc.nextLine();
+		System.out.println("hello world "+ name);
+
+	}
+
 }
