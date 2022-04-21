662d0c83b1c297d3234e9f7b6da077f462350c11
Bhupratim715 <1802048@che.buet.ac.bd>
2022-04-22 01:45:32 +0600

    running some code

--- Person.java
+++ Person.java
@@ -2,8 +2,8 @@ public class Person {
   
   public static void main(String[] args) {
     
-    System.out.println("I am a person, not a computer.");
-    System.out.println("hi there lets work together")
+    System.out.println("my first java program");
+    System.out.println("started learning java form 22 april");
     
   }
   
