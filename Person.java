5423a8595ada6373ca0a95bea2beb119a4a59a02
Bhupratim715 <1802048@che.buet.ac.bd>
2022-04-22 02:45:08 +0600

    tring new things

--- Person.java
+++ Person.java
@@ -1,8 +1,10 @@
+import java.util.Scanner;
 public class Person {
-  
+  static Scanner sc = new Scanner(System.in);
   public static void main(String[] args) {
-    
-    System.out.println("my first java program");
+    System.out.println("please enter your name\n");
+    String name = sc.nextLine();
+    System.out.println("hello "+name);
     
     
   }
