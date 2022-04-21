d79a7f82ccb56f82fd8d0e59cd484df94f5e6638
Bhupratim715 <1802048@che.buet.ac.bd>
2022-04-22 01:47:04 +0600

    playing some code

--- Person.java
+++ Person.java
@@ -3,7 +3,7 @@ public class Person {
   public static void main(String[] args) {
     
     System.out.println("my first java program");
-    System.out.println("started learning java form 22 april");
+    
     
   }
   
