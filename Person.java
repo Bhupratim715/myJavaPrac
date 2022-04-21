7ca61bce76bef7a27b34730740123d4f3d74ebd3
Bhupratim715 <1802048@che.buet.ac.bd>
2022-04-22 01:43:37 +0600

    my first java run

---
+++ Person.java
@@ -0,0 +1,10 @@
+public class Person {
+  
+  public static void main(String[] args) {
+    
+    System.out.println("I am a person, not a computer.");
+    System.out.println("hi there lets work together")
+    
+  }
+  
+}
